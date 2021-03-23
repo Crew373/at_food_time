41daa47f92b592187971f74c747dcc532ab4421f
Crew373 <crew.uw.373@gmail.com>
2021-03-23 14:03:32 +0900

    test: fix model test

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -7,7 +7,8 @@ class RestaurantTest < ActiveSupport::TestCase
 
   def setup
     @restaurant = Restaurant.new(name: "name", access: "access", address: "address",
-                                 open: "open", photo: "photo")
+                                 open: "open", photo: "photo", range: 1,
+                                 latitude: 34.67, longitude: 135.52)
   end
 
   test "should be valid" do
