9934f0ef9ef06e92407f5df9b259dc9c9846184f
Crew373 <crew.uw.373@gmail.com>
2021-03-23 12:50:19 +0900

    refactor: remove new action from controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -25,11 +25,6 @@ class RestaurantsController < ApplicationController
   end
 
 
-  def new
-    @restaurant = Restaurant.new
-  end
-
-
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
