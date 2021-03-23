44aa90b90a0a428b3fb3979c607d3fa593146caf
Crew373 <crew.uw.373@gmail.com>
2021-03-23 13:56:37 +0900

    test: add controller test

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -4,4 +4,24 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
+
+  test "should get show" do
+    get show_path
+    assert_response :success
+  end
+
+  test "should get top" do
+    get top_path
+    assert_response :success
+  end
+
+  test "should post search" do
+    post search_path
+    assert_response :success
+  end
 end
